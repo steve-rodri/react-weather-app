d94f39441f4fe66e9fdfa5479f51c9558ed711b5
steve20pro <steve20.pro@gmail.com>
2018-11-04 09:31:31 -0500

    completed main logic, can display weather data, needs css, and additional bonuses

--- weather/src/components/Main.js
+++ weather/src/components/Main.js
@@ -3,14 +3,102 @@ import React, { Component } from 'react'
 class Main extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      unitIsFahrenheit: true,
+    }
+    this.kelvinToCelsius = this.KelvinToCelsius.bind(this);
+    this.kelvinToFahrenheit = this.KelvinToFahrenheit.bind(this);
+    this.dataAvail = this.dataAvail.bind(this);
+    this.city = this.city.bind(this);
+    this.temp = this.Temp.bind(this);
+    this.checkCondtion = this.checkCondition.bind(this);
+    this.minMax = this.minMax.bind(this);
   }
 
+  dataAvail(){
+    return (
+      this.props.data.weather.length !== 0
+    )
+  }
+
+  city(){
+    return (
+      this.props.data.weather.name
+    )
+  }
+
+  kelvinToFahrenheit(temp) {
+    return (
+      (temp - 273.15) * (9/5) + 32
+    )
+  }
+
+  kelvinToCelsius(temp) {
+    return (
+      temp - 273.15
+    )
+  }
+
+  temp (){
+    if (this.dataAvail()) {
+      if (this.state.unitIsFahrenheit) {
+        return (
+          Math.round(this.KelvinToFahrenheit(this.props.data.weather.main.temp))
+        )
+      } else {
+        return (
+          Math.round(this.KelvinToCelsius(this.props.data.weather.main.temp))
+        )
+      }
+    }
+  }
+
+  minMax(){
+    if (this.dataAvail()) {
+      if (this.state.unitIsFahrenheit) {
+        const min = Math.round(this.KelvinToFahrenheit(this.props.data.weather.main.temp_min))
+        const max = Math.round(this.KelvinToFahrenheit(this.props.data.weather.main.temp_max))
+        return (
+          <div className= "minMax">
+            <div id="min">
+              <h6>Min</h6>
+              <p>{min}</p>
+            </div>
+            <div id="max">
+              <h6>Max</h6>
+              <p>{max}</p>
+            </div>
+          </div>
+        )
+      }
+    }
+  }
+
+  checkCondition () {
+    if (this.dataAvail()) {
+      const weather = this.props.data.weather.weather;
+      const weatherCondition = weather[0];
+      return (
+        weatherCondition.description
+      )
+    }
+  }
 
 
   render(){
     return(
       <div className="Main">
-        I Work
+
+        <h4>{this.city()}</h4>
+
+        <div className="temp">
+          {this.temp()}
+        </div>
+
+        <h5>{this.checkCondition()}</h5>
+
+        {this.minMax()}
+
       </div>
     );
   }
